8c8da534a3967e915194c46cca94fbe24e8092e0
ZhuangBill <ZhuangBillActual@gmail.com>
2019-01-10 13:14:37 +1100

    v1.1, Doesnt use eval()

--- calc.py
+++ calc.py
@@ -10,12 +10,29 @@ def conditions(a):
 acceptablefunctions = ("+","-","*","/")
 
 if len(sys.argv) == 4: #Sys.argv[0] returns file directory
-    if conditions(sys.argv[1]) and conditions(sys.argv[3]):
-        if sys.argv[2] in acceptablefunctions:
-            if sys.argv[2] == "/" and float(sys.argv[3]) == 0:
+    operand1 = sys.argv[1]
+    operand2 = sys.argv[3]
+    operator = sys.argv[2]
+    if conditions(operand1) and conditions(operand2):
+        if operator in acceptablefunctions:
+            if operator == "/" and float(operand2) == 0:
                 print("Cannot divide by zero")
             else:
-                print(eval(sys.argv[1]+sys.argv[2]+sys.argv[3])) #concatenantes string to be evaluated
+                #print(eval(operand1+operator+operand2)) #concatenantes string to be evaluated
+                if operator == "+":
+                    result = float(operand1) + float(operand2)
+                    print(result)
+                elif operator == "-":
+                    result = float(operand1) - float(operand2)
+                    print(result)
+                elif operator == "*":
+                    result = float(operand1) * float(operand2)
+                    print(result)
+                elif operator == "/"
+                    result = float(operand1) / float(operand2)
+                    print(result)
+                else:
+                    print("Error somewhere, you shouldnt see this message")
         else:
             print("Invalid operator")
     else:
