baae95628995f2660d7e6aff17823ce96ad5d62f
ZhuangBill <ZhuangBillActual@gmail.com>
2019-01-11 13:50:37 +1100

    Unfinished, Finished Hex to 64 but XOR not complete.

--- Crypto.py
+++ Crypto.py
@@ -1,10 +1,23 @@
-import codecs
+import codecs, base64, binascii
 #Hex to Base64 converted
-def hb64(message):
-	a = codecs.encode(codecs.decode(message, 'hex'), 'base64').decode()
-	print(a) 
+#Converter 1 -> Converts from String to string, then to byte then to
 def hh(message):
-	a = codecs.decode(message, 'hex')
-	b = codecs.encode(a,'base64')
-	print(b)
-#Why are there two junk letters in front and behind
+	a = codecs.decode(message, 'hex') #Returns a string
+	b = codecs.encode(a,'base64') #Convert to Base64, and returns a byte.
+	rmessage = b.strip().decode('ascii') #Convert string to ASCII and strips the b and trailing newline
+	print(rmessage)
+#Original decoded message is type 'byte' and hence has a trailing newline and header
+#Converter 2 -> Converts to B64 as a byte at all times:
+def h64(message):
+	asciimessage = binascii.unhexlify(message)
+	return base64.b64encode(asciimessage)
+#Fixed XOR decoder
+def convord(message):
+	rlist = []
+	rlist.append((ord(c)) for c in message)
+	print(rlist)
+convord("potato")
+def XOR(a, b):
+	#Convert message 1 and 2 to unicode
+
+XOR("1c0111001f010100061a024b53535009181c","686974207468652062756c6c277320657965")
