689e9863ebf539bbdf132b72de54aa757ac254d7
ZhuangBill <ZhuangBillActual@gmail.com>
2019-02-15 12:13:00 +1100

    P24 Solved, P23 still stuck

--- Euler Project Scribbles.py
+++ Euler Project Scribbles.py
@@ -10,4 +10,20 @@ def p22():
     toread.sort()
     #64 minus
     return sum([(sum((ord(c) - 64) for c in name) * (toread.index(name) + 1)) for name in toread ])
-#
+
+#Problem 23 T = ~30 m +
+def p23():
+    abundant = [number for
+                    number in range(1,28124-12)
+                    if (number < sum([n for n in range(1,number//2 + 1) if (number % n == 0)]))]
+    return abundant
+    removelist =  [sum(a,b) for a in abundant for b in abundant if ((a + b) < 28124)]
+    return removelist
+
+#Problem 24 T = 22:00
+def p24():
+    from itertools import permutations
+    a = permutations([0,1,2,3,4,5,6,7,8,9])
+    b = list(a)
+    b.sort()
+    return b[999999]
