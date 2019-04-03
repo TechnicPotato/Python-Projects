4635f7379811d40880abdebd96e39c29f4522012
ZhuangBill <bzhu2710@wfy85cd2.internal.usyd.edu.au>
2019-04-03 13:27:41 +1100

    Added further functionality to P19 Allowed daycount to be utilised.

--- Euler/P19.py
+++ Euler/P19.py
@@ -41,6 +41,42 @@ class mdate():
                         pass
             i += 1
         return (self.day,self.month,self.year, self.daycount)
+    def day_count(self,date_day,date_month,date_year):
+        # Generate while loop to do checking daily
+        i = 0
+        while not((self.day == date_day) and (self.month== date_month) and (self.year == date_year)):
+            # just in case
+            if self.year > date_year:
+                print("Something Wrong!")
+                break
+        # Run a check if it's the leap year once a year
+            if self.year % 100 == 0:
+                isleapyear = (self.year % 400 == 0)
+            else:
+                isleapyear = (self.year % 4 == 0) 
+            self.day += 1
+            self.daycount = (self.daycount + 1) % 7
+            # Checks for month and year
+            if isleapyear == True:
+                if self.day > leapdaycount[self.month]:
+                    self.day = 1
+                    self.month += 1
+                    if self.month > 12:
+                        self.month = 1
+                        self.year += 1
+                    else:
+                        pass
+            else:
+                if self.day > daycount[self.month]:
+                    self.day = 1
+                    self.month += 1
+                    if self.month > 12:
+                        self.month = 1
+                        self.year += 1
+                    else:
+                        pass
+            i += 1
+        return i
     def problem(self,days,set_day = 6):
         # Generate while loop to do checking daily
         i = 0
@@ -78,6 +114,7 @@ class mdate():
                 count += 1
         return count
 date1 = mdate(1,1,1901,2)
+print(date1.day_count(31,12,2000))
 print(date1.update(36524))
 print(date1.problem(36524))
 
