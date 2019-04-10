187f25ef7f44e4f8c747e2e113e52bd2fbea21bf
bzhu2710 <bzhu@uni.sydney.edu.au>
2019-04-10 21:30:04 +1000

    Task 3 Done F*ck this shit

--- Task3.c
+++ Task3.c
@@ -1,79 +1,131 @@
 #include <stdio.h>
 // Buffers and stuff
-char buf[32];
 int count = 0;
 int int_array[20];
+char buf[32];
+
 // Shitty string copier
-int copystr(char* dest, char* source){
-    
+int copystr(char* dest, const char* source){
+	int count = 0;
+	while (source[count]!=0){
+		dest[count] = source[count];
+		count ++;
+	}
+	dest[count] = 0;
+	return count;
+}
 
 // Passport Check Function
 int passport_check(char *string){
-    char code;
-    int danger_level
-    code = getchar();
-    if (code == EOF){
-        return 0;
+	char code;
+	int danger_level;
+	char buf[32];
+	char c;
+	int counter=0;
+	do{
+		c = getchar();
+		buf[counter] = c;
+		counter++;
+	} while(c != '\n' && c != EOF);
+	if(c==EOF){
+		return 0;
+	}
+	code = buf[0];
+	danger_level = buf[1];
+	if (code == 'O'){
+		if ((danger_level - '0') > 4){
+		copystr(string,"Sorry, Visa denied.");
         }
-    else if (code == 'O'){
-        danger_level = getchar() - '0';
-        if (danger_level > 4){
-            string = "Get Lost!"
-            }
-        }
-    return 1;
-    }
+		else {
+		copystr(string,"Hello World!");
+	}
+    	}
+    	else if (code == 'A'){
+	    	copystr(string, "Glory to Arstotzka!");
+    	}
+    	else if (code == 'R'){
+	    	copystr(string, "Go Away!");
+    	}
+    	else{
+	    	copystr(string, "INVALID PASSPORT");
+    	}
+    	return 1;
+}
+
 // Reading Age Function
 int read_age(void){
-    int c;
-    char age[10];
-    int age_position = 0;
-    // Loop to read int from stdin
-    do {
-        c = getchar();   
-        if (c!= 10){
-            age[age_position] = c;
-            age_position++;
-            }
-        } while (c != 10);
-    age[age_position] = '\0';
-    // Loop that converts age into int
-    int i = 0;
-    int n = 0;
-    while(age[i] >= '0' && age[i] <= '9'){
-        n = n * 10 + (age[i] - '0');
-        i ++; 
-        }
-    age[i] = '\0';
-    return n;
-    }
+//void read_age(){
+	int c;
+	char age[11];
+	int age_position = 0;
+	// Loop to read int from stdin
+	do {
+        	c = getchar();
+	        if (c!= 10){
+			age[age_position] = c;
+		        age_position++;
+        	}
+    	} while (c != 10);
+       age[age_position] = '\0';
+       // Loop that converts age into int
+       int i = 0;
+       int n = 0;
+       while(age[i] >= '0' && age[i] <= '9'){
+	       n = n * 10 + (age[i] - '0');
+	       i ++;
+    	}
+    	age[i] = '\0';
+    	return n;
+}
+
 // Averaging function
 void calculate_average(){
-    int c = 0;
-    int n = 0;
-    while (c < count){
-        n += int_array[c];
-        c++;
-        }
-    double average;
-    average = (double) n/count;
-    printf("Average age: %02f (from %d passports)\n",average, count);
-    }
+	int c = 0;
+	int n = 0;
+	while (c < count){
+		n += int_array[c];
+		c++;
+    	}
+	double average;
+	average = (double) n/count;
+	printf("Average age: %.02f (from %d passports)\n",average, count);
+}
+
+// Name parsing
+void read_name(){
+	int c;
+	int count2 = 0;
+	do{
+		c = getchar();
+		if (c == 10){
+			break;
+		}
+		buf[count2] = c;
+		count2 ++;
+	} while(c != 10);
+	buf[count2] = 0;
+}
 // Main Function
-int main(){  
-    int age;
-    int conditional;
-    int i = 0;
-    char buf2[32];
-    while (conditional == 1){
-        conditional = passport_check(buf2);
-        age = read_age();
-        int_array[count] = age;
-        count ++;
-        printf("Your age is %d.\n", age);
-        i++;
-        }
-    calculate_average();
-    printf("Done!\n");
-    return 0;
-    }
+int main(){
+	int age;
+	int conditional = 1;
+	int i = 0;
+	char buf2[32];
+	while (conditional == 1){
+		printf("Please enter passport (code, age, name in that order):\n");
+        	conditional = passport_check(buf2);
+			if(conditional == 0){
+				continue;
+			}
+		age = read_age();
+		read_name();
+		int_array[count] = age;
+		count ++;
+		printf("Dear %s, %s\n",buf,buf2);
+	//	printf("Your age is %d.\n", age);
+		i++;
+	}
+	calculate_average();
+	printf("Done!\n");
+	return 0;
+}
